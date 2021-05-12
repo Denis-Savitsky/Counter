12a0adb7229cfd975ab289417345bed2bd650359
Denis-Savitsky <denis_savickii96@mail.ru>
2021-05-13 01:46:49 +0300

    Add handling exceptions in controller

--- src/main/java/com/denis/counter/controller/CounterController.java
+++ src/main/java/com/denis/counter/controller/CounterController.java
@@ -1,8 +1,12 @@
 package com.denis.counter.controller;
 
+import com.denis.counter.exceptions.CounterAlreadyExistsException;
+import com.denis.counter.exceptions.NoSuchCounterException;
 import com.denis.counter.service.CounterService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Set;
 
@@ -14,22 +18,42 @@ public class CounterController {
 
     @PostMapping("/counters")
     public void createController(@RequestParam("name") String name) {
-        counterService.createNewCounter(name);
+        try {
+            counterService.createNewCounter(name);
+        } catch (CounterAlreadyExistsException ex) {
+            throw new ResponseStatusException(
+                    HttpStatus.CONFLICT, "Counter already exists");
+        }
     }
 
     @PutMapping("/counters")
     public void incrementCounterValue(@RequestParam("name") String name) {
-        counterService.incrementCounterValue(name);
+        try {
+            counterService.incrementCounterValue(name);
+        } catch (NoSuchCounterException ex) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Counter not found");
+        }
     }
 
     @GetMapping("/counters")
     public int getCounterValue(@RequestParam("name") String name) {
-        return counterService.getCounterValue(name);
+        try {
+            return counterService.getCounterValue(name);
+        } catch (NoSuchCounterException ex) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Counter not found");
+        }
     }
 
     @DeleteMapping("/counters")
     public void deleteCounter(@RequestParam("name") String name) {
-        counterService.deleteCounter(name);
+        try {
+            counterService.deleteCounter(name);
+        } catch (NoSuchCounterException ex) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Counter not found");
+        }
     }
 
     @GetMapping("/counters/value")
