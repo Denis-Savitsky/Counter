727cdd066a65b086ddd5b45765e4a077eced5e1d
Denis-Savitsky <denis_savickii96@mail.ru>
2021-05-13 01:33:14 +0300

    Make service use repository

--- src/main/java/com/denis/counter/service/CounterServiceImpl.java
+++ src/main/java/com/denis/counter/service/CounterServiceImpl.java
@@ -1,51 +1,59 @@
 package com.denis.counter.service;
 
+import com.denis.counter.exceptions.CounterAlreadyExistsException;
+import com.denis.counter.exceptions.NoSuchCounterException;
 import com.denis.counter.model.Counter;
-import lombok.val;
+import com.denis.counter.repository.CounterRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 @Service
-public class CounterServiceImpl implements CounterService{
+@RequiredArgsConstructor
+public class CounterServiceImpl implements CounterService {
 
-    private final ConcurrentHashMap<String, Counter> counters;
-
-    public CounterServiceImpl() {
-        counters = new ConcurrentHashMap<>();
-    }
+    private final CounterRepository counterRepository;
 
     @Override
     public void createNewCounter(String name) {
-        counters.putIfAbsent(name, new Counter(name, 0));
+        var counter = new Counter(name, 0);
+        if (counter.equals(counterRepository.addCounter(counter))) {
+            throw new CounterAlreadyExistsException();
+        }
     }
 
     @Override
     public void incrementCounterValue(String name) {
-        counters.computeIfPresent(name, (key, val) -> new Counter(key, val.getValue() + 1));
+        counterRepository.updateCounter(name)
+                .orElseThrow(NoSuchCounterException::new);
     }
 
     @Override
     public int getCounterValue(String name) {
-        return counters.get(name).getValue();
+        return counterRepository.getByName(name)
+                .map(Counter::getValue)
+                .orElseThrow(NoSuchCounterException::new);
     }
 
     @Override
     public void deleteCounter(String name) {
-        counters.remove(name);
+        counterRepository.deleteCounter(name)
+                .orElseThrow(NoSuchCounterException::new);
     }
 
     @Override
     public int getAccumulativeValue() {
-        return counters.values()
-                .stream()
+        return counterRepository.getAllCounters().stream()
                 .mapToInt(Counter::getValue)
                 .reduce(0, Integer::sum);
     }
 
     @Override
     public Set<String> getCountersNames() {
-        return counters.keySet();
+        return counterRepository.getAllCounters().stream()
+                .map(Counter::getName)
+                .collect(Collectors.toSet());
     }
 }
